f941e3d7a3dcedfcc904ba80aa4b285d8cb933e9
leticea <nascimento.leticeah@gmail.com>
2023-02-01 15:25:17 -0300

    criando evento e anexando imagem

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -36,7 +36,7 @@ class EventController extends Controller
 
             $extension = $requestImage->extension();
 
-            $imageName = md5($requestImage->image->getClientOriginalName() . strtotime("now")) . "." . $extension;
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
 
             $requestImage->move(public_path('img/events'), $imageName);
 
