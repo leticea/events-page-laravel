5003485e81e51fa34270cae98cc050a7afd05162
leticea <nascimento.leticeah@gmail.com>
2023-02-01 17:46:36 -0300

    criando método para fazer a busca

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,16 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="">
+    <form action="/" method="get">
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
 </div>
 <div id="events-container" class="col-md-12">
-    <h2>Próximos Eventos</h2>
+    @if ($search)
+        <h2>Buscando por: {{ $search }}</h2>
+    @else
+        <h2>Próximos Eventos</h2>
+    @endif
     <p class="subtitle">Veja os eventos dos próximos dias</p>
     <div id="cards-container" class="row">
         @foreach ($events as $event)
@@ -25,7 +29,9 @@
             </div>
         </div>
         @endforeach
-        @if (count($events) == 0)
+        @if (count($events) == 0 && $search)
+            <p>Não foi possível encontrar nenhum evento com {{ $search }}! <a href="/">Ver todos</a></p>
+        @elseif (count($events) == 0)
             <p>Não há eventos disponíveis</p>
         @endif
     </div>
