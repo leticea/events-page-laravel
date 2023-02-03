06f63d78d87b63a20c7f8ab697b499cc7c72af5a
leticea <nascimento.leticeah@gmail.com>
2023-02-03 12:50:57 -0300

    criando função de update

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -100,7 +100,22 @@ class EventController extends Controller
 
     public function update(Request $request) {
 
-        Event::findOrFail($request->id)->update($request->all());
+        $data = $request->all();
+
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $data['image'] = $imageName;
+        }
+
+        Event::findOrFail($request->id)->update($data);
 
         return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
     }
